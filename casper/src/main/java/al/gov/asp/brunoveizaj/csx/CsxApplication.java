0f30bfdab86f99ca94110828ad7ab3ad2793a1c9
Bruno <Bruno@DESKTOP-2DD5UCT>
2018-05-25 13:39:54 +0200

    Casper first commit

---
+++ casper/src/main/java/al/gov/asp/brunoveizaj/csx/CsxApplication.java
@@ -0,0 +1,12 @@
+package al.gov.asp.brunoveizaj.csx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsxApplication.class, args);
+	}
+}
---
+++ casper/src/test/java/al/gov/asp/brunoveizaj/csx/CsxApplicationTests.java
@@ -0,0 +1,16 @@
+package al.gov.asp.brunoveizaj.csx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
